f5d413916de3cd3378c8a4ed749532e13a4446a7
Jedidiah Ramos <143989730+jedidiahramos@users.noreply.github.com>
2024-04-03 10:37:39 +0800

    Copy-pasted updated Interpreter.java with double and integer operations

--- src/CODE/Interpreter.java
+++ src/CODE/Interpreter.java
@@ -55,6 +55,7 @@ public class Interpreter implements Expr.Visitor<Object> {
     private void checkNumberOperands(Token operator,
                                      Object left, Object right) {
         if (left instanceof Double && right instanceof Double) return;
+        else if (left instanceof Integer && right instanceof Integer) return;
 
         throw new RuntimeError(operator, "Operands must be numbers.");
     }
@@ -72,19 +73,54 @@ public class Interpreter implements Expr.Visitor<Object> {
         switch (expr.operator.type) {
             case GREATER:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left > (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left > (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left > (Integer) right;
+                }
+
             case GREATER_EQUAL:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left >= (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left >= (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left >= (Integer) right;
+                }
+
             case LESS:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left < (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left < (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left < (Integer) right;
+                }
+
             case LESS_EQUAL:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left <= (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left <= (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left <= (Integer) right;
+                }
+
             case MINUS:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left - (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left - (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left - (Integer) right;
+                }
+
             case PLUS:
                 if (left instanceof Double && right instanceof Double) {
                     return (double)left + (double)right;
@@ -97,10 +133,24 @@ public class Interpreter implements Expr.Visitor<Object> {
                 break;
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left / (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left / (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left / (Integer) right;
+                }
+
             case STAR:
                 checkNumberOperands(expr.operator, left, right);
-                return (double)left * (double)right;
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left * (double)right;
+                }
+
+                if (left instanceof Integer && right instanceof Integer) {
+                    return (Integer)left * (Integer) right;
+                }
+
             case NOT_EQUAL: return !isEqual(left, right);
             case EQUAL_EQUAL: return isEqual(left, right);
         }
