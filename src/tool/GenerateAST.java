01f581a3689199b3f7d47508f4b4427ef3ff6dc3
Jedidiah Ramos <143989730+jedidiahramos@users.noreply.github.com>
2024-04-25 11:11:28 +0800

    Added conditional code in GenerateAST.java

--- src/tool/GenerateAST.java
+++ src/tool/GenerateAST.java
@@ -16,15 +16,19 @@ public class GenerateAST {
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
+                "Logical  : Expr left, Token operator, Expr right",
                 "Unary    : Token operator, Expr right",
                 "Variable : Token name"
         ));
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
+                "If         : Expr condition, Stmt thenBranch," +
+                        " Stmt elseBranch",
                 "Print      : Expr expression",
                 "Var        : Token name, Expr initializer, Token type",
-                "Scan       : Token name"
+                "Scan       : Token name",
+                "While      : Expr condition, Stmt body"
         ));
     }
     private static void defineAst(
