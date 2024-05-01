68cfdd3e0dc88421d62180a3cce32d9249eb08a7
Jedidiah Ramos <143989730+jedidiahramos@users.noreply.github.com>
2024-05-01 23:28:33 +0800

    Updated Stmt.java with if statements

--- src/CODE/Stmt.java
+++ src/CODE/Stmt.java
@@ -35,6 +35,20 @@ abstract class Stmt {
 
     final Expr expression;
   }
+  static class If extends Stmt {
+    If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+      this.condition = condition;
+      this.thenBranch = thenBranch;
+      this.elseBranch = elseBranch;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) { return visitor.visitIfStmt(this); }
+
+    final Expr condition;
+    final Stmt thenBranch;
+    final Stmt elseBranch;
+  }
   static class Print extends Stmt {
     Print(Expr expression) {
       this.expression = expression;
